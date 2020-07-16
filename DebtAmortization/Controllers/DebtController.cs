883c41dd5b959b7b9024a379cdcec821bf813f13
atvfool <atvfool@gmail.com>
2020-07-16 16:21:48 -0400

    Added Add page and beginning of edit page
    Also added view and delete buttons for debts.

--- DebtAmortization/Controllers/DebtController.cs
+++ DebtAmortization/Controllers/DebtController.cs
@@ -7,6 +7,8 @@ using DebtAmortization.Models;
 using DebUtilities;
 using DebtData;
 using Remotion.Linq.Parsing.Structure.NodeTypeProviders;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using System.Net;
 
 namespace DebtAmortization.Controllers
 {
@@ -18,24 +20,68 @@ namespace DebtAmortization.Controllers
             return View();
         }
 
-        public IActionResult Results()
+        public IActionResult Results(int ID)
         {
             DebtModel debtModel = new DebtModel();
 
-            string Name = Request.Form["txtName"];
+            // Save code goes here
+            Data data = new Data();
+            Debt debt = data.connection.FirstOrDefault<Debt>("WHERE ID = @0", ID);
+
+            if(debt != null)
+            {
+                Amortization amortization = new Amortization(debt);
+
+                debtModel.Name = debt.Name;
+                debtModel.Amount = debt.Amount;
+                debtModel.APR = debt.Interest;
+                debtModel.Payment = debt.MinPayment;
+                debtModel.AmortizationSchedule = amortization;
+            }
+            
+
+            return View(debtModel);
+        }
+
+        public IActionResult Edit(int ID)
+        {
+            DebtModel debtModel = new DebtModel();
+
+            // Save code goes here
+            Data data = new Data();
+            Debt debt = data.connection.FirstOrDefault<Debt>("WHERE ID = @0", ID);
+
+            if (debt != null)
+            {
+                debtModel.Name = debt.Name;
+                debtModel.Amount = debt.Amount;
+                debtModel.APR = debt.Interest;
+                debtModel.Payment = debt.MinPayment;
+            }
+
+
+            return View(debtModel);
+        }
+
+
+        [HttpPost]
+        public void Add(DebtModel debtModel)
+        {
+            string Name = debtModel.Name;
             decimal Amount = 0;
             decimal APR = 0;
             decimal Payment = 0;
 
-            decimal.TryParse(Request.Form["numAmount"].ToString(), out Amount);
-            decimal.TryParse(Request.Form["numAPR"].ToString(), out APR);
-            decimal.TryParse(Request.Form["numPayment"].ToString(), out Payment);
+            decimal.TryParse(debtModel.Amount.ToString(), out Amount);
+            decimal.TryParse(debtModel.APR.ToString(), out APR);
+            decimal.TryParse(debtModel.Payment.ToString(), out Payment);
 
             APR /= 100;
 
             // Save code goes here
             Data data = new Data();
-            Debt debtToAdd = new Debt() {
+            Debt debtToAdd = new Debt()
+            {
                 Name = Name,
                 Amount = Amount,
                 DueDay = 1,
@@ -45,15 +91,55 @@ namespace DebtAmortization.Controllers
 
             data.Insert(debtToAdd);
 
-            Amortization amortization = new Amortization(debtToAdd);
 
-            debtModel.Name = Name;
-            debtModel.Amount = Amount;
-            debtModel.APR = APR;
-            debtModel.Payment = Payment;
-            debtModel.AmortizationSchedule = amortization;
-
-            return View(debtModel);
+            Response.Redirect("/Debt/List");
         }
+
+        public IActionResult List()
+        {
+            List<DebtModel> debtModels = new List<DebtModel>();
+
+            Data data = new Data();
+            List<Debt> debts = data.connection.Fetch<Debt>("SELECT * FROM Debt WHERE UserID = 1");
+
+            debts.ForEach(x => debtModels.Add(new DebtModel
+            {
+                ID = x.ID,
+                Name = x.Name,
+                Amount = x.Amount,
+                APR = x.Interest,
+                Payment = x.MinPayment,
+            }));
+
+            return View(debtModels);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAccount(string accountID)
+        {
+            ActionResult result;
+
+            Data data = new Data();
+            if(data.connection.Delete<Debt>("WHERE ID = @0", accountID) > 0)
+            {
+                result = Json(new {success =true, status = "success", message = "Deleted" });
+
+                Response.StatusCode = (int)HttpStatusCode.OK;
+            }
+            else
+            {
+                result = Json(new {success=false, status = "error", message = "Error" });
+
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+
+            return result;
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
     }
 }
--- DebtAmortization/Models/DebtModel.cs
+++ DebtAmortization/Models/DebtModel.cs
@@ -9,10 +9,23 @@ namespace DebtAmortization.Models
 {
     public class DebtModel
     {
+        [Microsoft.AspNetCore.Mvc.HiddenInput]
+        public int ID { get; set; }
+        [Display(Name="Name")]
+        [Required(ErrorMessage ="Debt Name is required.")]
         public string Name { get; set;}
+
+        [Display(Name = "Amount")]
         [DisplayFormat(DataFormatString = "{0:C}")]
+        [Required(ErrorMessage = "Amount is required.")]
         public decimal Amount { get; set; }
+
+        [Display(Name = "APR")]
+        [Required(ErrorMessage = "Interest is required.")]
         public decimal APR { get; set; }
+
+        [Display(Name = "Payment")]
+        [Required(ErrorMessage = "Payment is required.")]
         public decimal Payment { get; set; }
         public Amortization AmortizationSchedule { get; set; }
     }
